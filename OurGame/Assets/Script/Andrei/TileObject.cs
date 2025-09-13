e17d33589fb80f84418c9cb619b51689dba2908f
Andrei Emelianov <andrey2602@yahoo.com>
2025-09-13 12:37:48 +0200

    Гит игнор

---
+++ OurGame/Assets/Script/Andrei/TileObject.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileObject : MonoBehaviour
+{
+    public bool ocuppied = false;
+    public Collider2D collider;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
