bdd1dcd66530d99a1bfe567f6f509163f4c177d5
Andrei Emelianov <andrey2602@yahoo.com>
2025-09-14 20:55:15 +0200

    MainMenu

--- OurGame/Assets/Script/MenuManager.cs
+++ OurGame/Assets/Script/MenuManager.cs
@@ -8,64 +8,32 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private GameObject levelCompleteContainer;
 
     // This is the flag that controls the listener in Update()
-    private bool isListeningForNextSceneTap = false;
-    // --- END NEW LOGIC ---
+    
 
+    private bool gameStarted = false;
 
-    public void StartGame()
+    private void Start()
     {
-        // Загружаем сцену по имени. Убедитесь, что имя 'GameScene'
-        // совпадает с именем вашей игровой сцены.
-        SceneManager.LoadScene("PrototypeFinal");
+        levelCompleteContainer.SetActive(false);
     }
 
     private void Update()
     {
-        // If our flag is false, we are not listening, so do nothing.
-        if (!isListeningForNextSceneTap)
-        {
-            return;
-        }
 
-        // If the flag IS true, listen for the input.
-        // Input.GetMouseButtonDown(0) works for BOTH a mouse click AND a screen touch.
         if (Input.GetMouseButtonDown(0))
         {
-            // Input detected!
 
-            // 1. IMPORTANT: Set the flag back to false immediately.
-            // This stops the Update loop from listening anymore and prevents
-            // this code from accidentally running twice.
-            isListeningForNextSceneTap = false;
-
-            // 2. (Optional but good practice) Hide the container you just showed.
-            if (levelCompleteContainer != null)
+            if (gameStarted)
             {
-                levelCompleteContainer.SetActive(false);
+                SceneManager.LoadScene("PrototypeFinal");
             }
-
-            // 3. Load the scene you requested.
-            SceneManager.LoadScene("PrototypeFinal");
         }
     }
 
     public void ShowLevelCompleteScreen()
     {
-        // 1. Activate the UI parent object (which contains your sprite and text).
-        // Make sure this object is set to INACTIVE by default in the scene.
-        if (levelCompleteContainer != null)
-        {
-            levelCompleteContainer.SetActive(true);
-        }
-        else
-        {
-            Debug.LogWarning("LevelCompleteContainer is not assigned in the GameManager Inspector!");
-        }
-
-        // 2. Set the flag to TRUE. This "arms" the Update() loop,
-        // which will now start listening for the tap.
-        isListeningForNextSceneTap = true;
+        gameStarted = true;
+        levelCompleteContainer.SetActive(true);
     }
-    // --- END NEW FUNCTION ---
 
 }
